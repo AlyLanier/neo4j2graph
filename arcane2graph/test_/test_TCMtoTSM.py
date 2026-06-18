6f8a1b0b322f2490f4860f85a6acfe19a8e2f71b
Alyssia Lanier <alyssia.lanier@cea.fr>
2026-06-18 11:39:45 +0200

    generalized common tests and added TCM and TSM -related tests

---
+++ arcane2graph/test_/test_TCMtoTSM.py
@@ -0,0 +1,104 @@
+from TCMtoTSM import *
+from test_.test_graphs import *
+from itertools import combinations
+
+def test_structure(tsm):
+    vn, sn, ce, se = tsm.get_model()
+    test_node_types((vn, VNode), (sn, SNode))
+    test_edge_types(ce, Edge, [lambda edge: isinstance(edge.source(), (VNode, SNode)), lambda edge: type(edge.source()) == type(edge.target())])
+    test_edge_types(se, Edge, [lambda edge: isinstance(edge.source(), (VNode)) and isinstance(edge.target(), SNode)])
+
+def test_validity(tsm):
+    vn, sn, ce, se = tsm.get_model()
+
+    test_duplicate(vn, sn, ce, se) # no duplicates in lists
+    test_unique_root((sn, ce)) #only one root for specification side
+    test_nodes_in_one_edge((vn+sn, ce), (vn+sn, se)) # node is in at least 1 edge and all nodes from edges are in the node list
+    test_equal_edges(ce, se)# 2 edges cannot have the same source and target at the same time
+    # an edge source is not a leaf
+    for edge in ce:
+        if isinstance(edge.source(), VNode):assert edge.source().val() is None
+        else:                               assert edge.source().stype() in NODE_COMPOSITE_TYPES
+    test_unique_parent((sn, ce)) # a node has at most 1 parent for specification ones
+    test_exact_unique_child((vn, se)) # a value node is specified by only one spec node
+    test_vn_sn_type_verification(se) # all value nodes have their values of the type notified in their spec node
+    test_vn_child_spec_is_vn_spec_child(tsm) # proposition 5 of research paper : if vn contains vn', spec(vn) contains spec(vn')
+    test_acyclic_extended(tsm) # acyclic graph
+    
+def test_acyclic_extended(tsm):
+    vn, sn, ce, se = tsm.get_model()
+
+    # edge does not have the same source and target
+    for edge in ce:
+        assert edge.source() != edge.target()
+    
+    # for spec nodes
+    # we know that :    there is exactly 1 root
+    #                   each node only has 1 parent
+    # so we just need to test for rings / if we follow the path from the root, 
+    # we must catch all nodes and not find a seen_node
+    spec_containment_edges = tsm.get_containment_specification_edges()
+    root = TCM.search_root(spec_containment_edges, start_edge = len(spec_containment_edges)//2)
+    seen_nodes = [root]
+    test_acyclic_rec(root, spec_containment_edges, seen_nodes)
+    assert len(seen_nodes) == len(sn)
+
+    # for value nodes
+    # we know nothing, so for each root, we must show that a node from depth n doesn't have an edge going to depth m < n
+    # in reality here, depth m = depth n + 1
+    # and that each vn node is found by traversing each root
+    val_containment_edges = tsm.get_containment_value_edges()
+    roots = [root for root in vn if TCM.find_parents(root, val_containment_edges) == []]
+    verified_nodes = {} # node to depth
+    for root in roots:
+        depth_counter = 0
+        test_acyclic_extended_rec(root, val_containment_edges, verified_nodes, depth_counter)
+    assert len(verified_nodes) == len(vn)
+
+
+def test_acyclic_rec(node, edges, seen_nodes):
+    node_children = TCM.find_children(node, edges)
+    for child in node_children:
+        assert child not in seen_nodes
+        seen_nodes.append(child)
+        test_acyclic_rec(child, edges, seen_nodes)
+
+def test_acyclic_extended_rec(node, edges, verified_nodes, depth_counter):
+    if node in verified_nodes:
+        assert verified_nodes[node] >= depth_counter # in reality verified_nodes[node] == depth_counter
+        return
+    node_children = TCM.find_children(node, edges)
+    for child in node_children:
+        test_acyclic_extended_rec(child, edges, verified_nodes, depth_counter+1)
+    verified_nodes[node] = depth_counter
+
+def test_vn_sn_type_verification(spec_edges):
+    for edge in spec_edges:
+        # TODO delete first condition when None value leaf nodes are removed from the graph
+        assert edge.source().val() == None or (edge.source().val() == None and edge.target().stype() in NODE_COMPOSITE_TYPES) or isinstance(edge.source().val(), edge.target().stype())
+
+def test_vn_child_spec_is_vn_spec_child(tsm):
+    vce, sce = tsm.get_containment_value_edges(), tsm.get_containment_specification_edges()
+    for edge in vce:
+        assert len(TCM.find_edges(sce, tsm.spec(edge.source()), tsm.spec(edge.target()))) == 1
+
+
+
+def test_tsm(tsm):
+    test_structure(tsm)
+    test_validity(tsm)
+
+
+
+json_path = "arc_json"
+processed_json = []
+for filename in os.listdir(json_path):
+    if filename.endswith(".json"):
+        file_path = os.path.join(json_path, filename)
+        processed_json.append(TCM(file_path, 'mahyco'))
+tsm_combinations = list(combinations(processed_json, len(processed_json)-1))[::2]
+for comb in tsm_combinations:
+    tsm = TSM(list(comb))
+    test_tsm(tsm)
+print("ALL tests validated")
+    
--- arcane2graph/test_/test_jsonToTCM.py
+++ arcane2graph/test_/test_jsonToTCM.py
@@ -1,4 +1,5 @@
 from jsonToTCM import *
+from test_.test_graphs import *
 
 #################### TESTS ######################""
 
@@ -6,37 +7,30 @@ def test_node(node):
     assert node.val() is None or node._type is None
 
 def test_structure(tcm):
-    nodes = tcm.get_nodes()
-    edges = tcm.get_edges()
+    nodes, edges = tcm.get_model()
 
+    test_node_types(nodes, Node)
     for obj in nodes:
-        assert isinstance(obj, Node)
         test_node(obj)
     
-    for obj in edges:
-        assert isinstance(obj, Edge)
-        assert isinstance(obj.target(), Node)
-        assert isinstance(obj.source(), Node)
+    test_edge_types(edges, Edge, [lambda edge: isinstance(edge.source(), Node) and isinstance(edge.target(), Node)])
 
 def test_validity(tcm):
-    nodes = tcm.get_nodes()
-    edges = tcm.get_edges()
-    nodes_in_edges = set(nodes_from_edges(edges))
+    nodes, edges = tcm.get_model()
 
-    assert len(nodes) == len(nodes_in_edges)
-
+    test_duplicate(nodes, edges) # no duplicate node
+    test_unique_root((nodes, edges)) # only one root
+    test_nodes_in_one_edge((nodes, edges)) # node is in at least 1 edge and all nodes from edges are in the node list
+    # a node must have a value type in those 2 sets
     for node in nodes:
         assert isinstance(node.val(), NODE_SIMPLE_TYPES) or node.get_type() in NODE_COMPOSITE_TYPES
-        assert node in nodes_in_edges
-
+    # an edge source is not a leaf
     for edge in edges:
-        src = edge.source()
-        tgt = edge.target()
-        assert src.val() is None and src.get_type() in NODE_COMPOSITE_TYPES
-
-        for other_edge in edges: #TODO check si pas 2 même edges 
-            pass
-    
+        assert edge.source().val() is None and edge.source().get_type() in NODE_COMPOSITE_TYPES
+    # 2 edges cannot have the same source and target at the same time
+    test_equal_edges(edges)
+    # a node has at most 1 parent
+    test_unique_parent((nodes, edges))
     # a map node has all its children with distinct names
     # a list node has all its children with identical names to it
     for node in nodes:
@@ -50,25 +44,44 @@ def test_validity(tcm):
                 assert len(children) == len(children_names)
     
     test_acyclic(tcm)
-    
+
 
 def test_acyclic(tcm):
     nodes = tcm.get_nodes()
     edges = tcm.get_edges()
 
+    # edge does not have the same source and target
     for edge in edges:
-        assert edge.source() != edge.target() # made on object id as __eq__ or __neq__ was not defined for Node
+        assert edge.source() != edge.target()
+    
+    # we know that :    there is exactly 1 root
+    #                   each node only has 1 parent
+    # so we just need to test for rings / if we follow the path from the root, 
+    # we must catch all nodes and not find a seen_node
+    root = tcm.search_root(edges, start_edge = len(edges)//2)
+    seen_nodes = [root]
+    test_acyclic_rec(root, edges, seen_nodes)
+    assert len(seen_nodes) == len(nodes)
 
+def test_acyclic_rec(node, edges, seen_nodes):
+    node_children = TCM.find_children(node, edges)
+    for child in node_children:
+        assert child not in seen_nodes
+        seen_nodes.append(child)
+        test_acyclic_rec(child, edges, seen_nodes)
 
-
-
-
+def test_tcm(tcm):
+    test_structure(tcm)
+    test_validity(tcm)
         
 
-################### Utils ####################
 
-def nodes_from_edges(edge_list):
-    for edge in edge_list:
-        yield edge.source()
-        yield edge.target()
+json_path = "arc_json"
+for filename in os.listdir(json_path):
+    if filename.endswith(".json"):
+        file_path = os.path.join(json_path, filename)
+        print(file_path)
+        tcm = TCM(file_path, 'mahyco')
+        test_tcm(tcm)
+print("ALL tests have been validated")
     
