8b91270147ee8b354fb9931982025ecc4dbde076
Alyssia Lanier <alyssia.lanier@cea.fr>
2026-06-23 16:45:16 +0200

    modified validation tests for db to either validate the db TSM by the query or by constructing its TSM locally and then testing it as a tsm

--- arcane2graph/test_/test_DbToTSM.py
+++ arcane2graph/test_/test_DbToTSM.py
@@ -2,7 +2,7 @@ from Neo4jGraphFunctions import GraphFunctions
 from neo4j import GraphDatabase
 from test_.test_TCMtoTSM import test_tsm
 
-def validate_db():
+def validate_db(by_query = True):
     URI = "bolt://localhost:7687"
     AUTH = ("neo4j", "password")
     DB_NAME = AUTH[0]
@@ -11,8 +11,9 @@ def validate_db():
         driver.verify_connectivity()
 
         with driver.session(database = DB_NAME) as session:
-            tsm = GraphFunctions.TSM_from_db(session)
-            db_validity = session.run(GraphFunctions.Db_Validity_query()).single()
-    assert db_validity[0] == True
-    test_tsm(tsm)
+            if by_query:    db_validity = session.run(GraphFunctions.Db_Validity_query()).single()
+            else:           tsm = GraphFunctions.TSM_from_db(session)
+    
+    if by_query:    assert db_validity[0] == True
+    else:           test_tsm(tsm)
     print('ALL tests validated')
